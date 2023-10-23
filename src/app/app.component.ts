bdec32c35896921882416d64129adc49e875cbc2
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2023-10-23 16:12:15 +0200

    getMouseCoords() method implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ydd-portfolio-angular';
+
+  ngOnInit(): void {
+    this.getMouseCoords();
+  }
+
+  getMouseCoords() {
+    const mousePosText = document.getElementById('mouse-pos');
+    let mousePos = {
+      x: 0,
+      y: 0,
+    };
+
+    window.addEventListener('mousemove', (event) => {
+      mousePos = {
+        x: event.clientX,
+        y: event.clientY,
+      };
+      mousePosText!.textContent = `(${mousePos.x}, ${mousePos.y})`;
+    });
+  }
 }
