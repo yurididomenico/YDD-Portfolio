35a31da3951f5e3f50685a4c05f63fdf0b5eb63c
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2023-10-23 12:28:24 +0200

    loader workinprogress dots implemented

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
-  styleUrls: ['./main.component.scss']
+  styleUrls: ['./main.component.scss'],
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
+  test: string = '';
 
+  ngOnInit(): void {
+    setInterval((): void => {
+      this.test += '.';
+
+      if (this.test.length > 3) {
+        this.test = '';
+      }
+    }, 1000);
+  }
 }
