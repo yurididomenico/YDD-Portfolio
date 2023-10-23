bca2f686aae15c53b5abf7d7b94a2b24154ae7e6
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2023-10-23 19:26:35 +0200

    About: style

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -3,8 +3,9 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.scss']
+  styleUrls: ['./about.component.scss'],
 })
 export class AboutComponent {
-
+  description =
+    'Dopo anni dedicati alla grafica 3d mi sono approcciato al mondo dello sviluppo web, acquisendo successivamente competenze tecniche sia su lato Front-end che Back-end.';
 }
