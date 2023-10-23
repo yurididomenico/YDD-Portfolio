f0075be6b9628c1e8f1d49fd24c6db759ef6a62f
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2023-10-23 20:13:14 +0200

    Print method implemented

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-about',
   templateUrl: './about.component.html',
   styleUrls: ['./about.component.scss'],
 })
-export class AboutComponent {
+export class AboutComponent implements OnInit {
   description =
     'Dopo anni dedicati alla grafica 3d mi sono approcciato al mondo dello sviluppo web, acquisendo successivamente competenze tecniche sia su lato Front-end che Back-end.';
+  listLibraries =
+    'Ho iniziato il mio percorso imparando i seguenti linguaggi, framework e librerie:';
+
+  currentText = '';
+  currentIndex = 0;
+
+  ngOnInit() {
+    this.typeDescription();
+  }
+
+  typeDescription() {
+    const printString = document.querySelector('.description');
+    if (this.currentIndex < this.description.length) {
+      this.currentText += this.description.charAt(this.currentIndex);
+      this.currentIndex++;
+      setTimeout(() => {
+        printString!.textContent = this.currentText;
+        this.typeDescription();
+      }, 40);
+    }
+  }
 }
