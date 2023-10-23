4a5d8571c9995e18fa9cb73a9433c280bc75e241
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2023-10-23 11:51:39 +0200

    Set Workspace
    header, main, line components created.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './components/header/header.component';
+import { MainComponent } from './components/main/main.component';
+import { LineComponent } from './components/line/line.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    MainComponent,
+    LineComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/line/line.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-line',
+  templateUrl: './line.component.html',
+  styleUrls: ['./line.component.scss'],
+})
+export class LineComponent {}
