228ee33b7511a7b266661f85e7b45a4414c6ec9b
Yuri Di Domenico <113028513+yurididomenico@users.noreply.github.com>
2023-10-25 12:44:20 +0200

    test particles implemented

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -25,6 +25,19 @@ export class AppComponent implements OnInit {
         x: event.clientX,
         y: event.clientY,
       };
+
+      const testDiv = document.createElement('div');
+
+      testDiv.style.position = 'absolute';
+      testDiv.style.height = '4px';
+      testDiv.style.width = '4px';
+      testDiv.style.backgroundColor = 'red';
+      testDiv.style.zIndex = '1000';
+
+      testDiv.style.top = mousePos.y + 'px';
+      testDiv.style.left = mousePos.x + 'px';
+      document.getElementsByClassName('container')[0].appendChild(testDiv);
+
       mousePosText!.textContent = `(${mousePos.x}, ${mousePos.y})`;
     });
   }
