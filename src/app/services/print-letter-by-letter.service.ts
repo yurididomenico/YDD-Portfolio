c81db6177e0dfb1bd40b23e032ef6b22807b668d
yurididomenico <yurididomenico93@gmail.com>
2024-03-06 18:53:59 +0100

    fix print-letter-by-letter function

--- src/app/services/print-letter-by-letter.service.ts
+++ src/app/services/print-letter-by-letter.service.ts
@@ -6,11 +6,12 @@ import { Injectable } from '@angular/core';
 export class PrintLetterByLetterService {
   descriptions = {
     description:
-      'Dopo anni dedicati alla grafica 3d mi sono approcciato al mondo dello sviluppo web, acquisendo successivamente competenze tecniche sia su lato Front-end che Back-end.',
+      // 'Dopo anni dedicati alla grafica 3d mi sono approcciato al mondo dello sviluppo web, acquisendo successivamente competenze tecniche sia su lato Front-end che Back-end.',
+      'Mi chiamo Yuri e sono un appassionato del mondo della programmazione.\nHo 30 anni, sono fortemente motivato a perseguire questa strada e disponibile ad essere assunto con contratto di apprendistato.\nNon vedo l\'ora di affinare le mie abilità di programmatore e rendere questa passione un vero e proprio lavoro.',
     listLibraries:
       'Ho iniziato il mio percorso imparando i seguenti linguaggi, framework e librerie:',
     languages:
-      'HTML, CSS, JavaScript, PHP, Sass, Bootstrap, Laravel, Vue, Angular.',
+      'HTML, CSS, JavaScript, PHP, Sass, Bootstrap, Laravel, Vue, Angular, Git.',
   };
   currentText = '';
   currentIndex = 0;
@@ -18,15 +19,19 @@ export class PrintLetterByLetterService {
   constructor() {}
 
   printLetterByLetter(printTo: string, desc: string) {
+    const targetPrint = document.getElementsByClassName(printTo)[0];
+
     if (this.currentIndex < desc.length) {
       this.currentText = desc.charAt(this.currentIndex);
 
-      document.getElementsByClassName(printTo)[0].innerHTML += this.currentText;
+      if (targetPrint != null) {
+        targetPrint.innerHTML += this.currentText;
+      }
 
       setTimeout(() => {
         this.currentIndex++;
         this.printLetterByLetter(printTo, desc);
-      }, 50);
+      }, 30);
     }
   }
 }
