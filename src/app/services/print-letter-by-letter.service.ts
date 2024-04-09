75f1ecb45b4b826c38664f671857658786acb0ac
yurididomenico <yurididomenico93@gmail.com>
2024-04-09 11:46:25 +0200

    printLetter

--- src/app/services/print-letter-by-letter.service.ts
+++ src/app/services/print-letter-by-letter.service.ts
@@ -7,7 +7,7 @@ export class PrintLetterByLetterService {
   descriptions = {
     description:
       // 'Dopo anni dedicati alla grafica 3d mi sono approcciato al mondo dello sviluppo web, acquisendo successivamente competenze tecniche sia su lato Front-end che Back-end.',
-      'Mi chiamo Yuri e sono un appassionato del mondo della programmazione.\nHo 30 anni, sono fortemente motivato a perseguire questa strada e disponibile ad essere assunto con contratto di apprendistato.\nNon vedo l\'ora di affinare le mie abilità di programmatore e rendere questa passione un vero e proprio lavoro.',
+      'Mi chiamo Yuri e sono un appassionato del mondo della programmazione.\nHo 30 anni, sono fortemente motivato a perseguire questa strada e disponibile ad essere assunto con contratto di tirocinio o stage.\nNon vedo l\'ora di affinare le mie abilità di programmatore e rendere questa passione un vero e proprio lavoro.',
     listLibraries:
       'Ho iniziato il mio percorso imparando i seguenti linguaggi, framework e librerie:',
     languages:
