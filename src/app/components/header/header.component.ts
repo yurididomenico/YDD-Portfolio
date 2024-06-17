a3c1647a4abc456addb3aaedd57abd811a240127
yurididomenico <yurididomenico93@gmail.com>
2024-06-17 22:51:04 +0200

    Particle effect on mouse hover disabled

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -12,6 +12,7 @@ export class HeaderComponent {
   constructor(private renderer: Renderer2, private elementRef: ElementRef) {}
 
   showHideDropdown() {
+    console.log("DIOCANE");
     const linksDropdown = this.elementRef.nativeElement.querySelector('.linksDropdown');
     this.renderer.setStyle(linksDropdown, 'top', this.divHeight + 'px');
     this.renderer.setStyle(linksDropdown, 'right', 5 + 'vw');
--- src/app/pages/homepage/homepage.component.ts
+++ src/app/pages/homepage/homepage.component.ts
@@ -8,12 +8,12 @@ import { Component, OnDestroy, OnInit } from '@angular/core';
 export class HomepageComponent implements OnDestroy {
   cursor = '';
 
-  private mouseMoveListener: (event: MouseEvent) => void;
+  // private mouseMoveListener: (event: MouseEvent) => void;
   private intervalCursor!: number;
 
   constructor() {
-    this.mouseMoveListener = this.getMouseCoords.bind(this);
-    window.addEventListener('mousemove', this.mouseMoveListener);
+    // this.mouseMoveListener = this.getMouseCoords.bind(this);
+    // window.addEventListener('mousemove', this.mouseMoveListener);
   }
 
   ngOnInit() {
@@ -27,34 +27,34 @@ export class HomepageComponent implements OnDestroy {
   }
 
   ngOnDestroy() {
-    window.removeEventListener('mousemove', this.mouseMoveListener);
-    clearInterval(this.intervalCursor);
+    // window.removeEventListener('mousemove', this.mouseMoveListener);
+    // clearInterval(this.intervalCursor);
   }
 
-  private getMouseCoords(event: MouseEvent) {
-    const selectContainer = document.getElementsByClassName('container')[0];
-    const mousePos = {
-      x: event.clientX,
-      y: event.clientY,
-    };
+  // private getMouseCoords(event: MouseEvent) {
+  //   const selectContainer = document.getElementsByClassName('container')[0];
+  //   const mousePos = {
+  //     x: event.clientX,
+  //     y: event.clientY,
+  //   };
 
-    const dot = document.createElement('div');
-    dot.style.cssText = `
-      position: absolute;
-      height: 5px;
-      width: 5px;
-      background-color: #1daa1b;
-      border-radius: 5px;
-      top: ${mousePos.y}px;
-      left: ${mousePos.x}px;
-      opacity: 0.5;
-      z-index: 1000;
-    `;
+  //   const dot = document.createElement('div');
+  //   dot.style.cssText = `
+  //     position: absolute;
+  //     height: 5px;
+  //     width: 5px;
+  //     background-color: #1daa1b;
+  //     border-radius: 5px;
+  //     top: ${mousePos.y + 10}px;
+  //     left: ${mousePos.x}px;
+  //     opacity: 0.5;
+  //     z-index: 1000;
+  //   `;
 
-    selectContainer.appendChild(dot);
+  //   selectContainer.appendChild(dot);
 
-    setTimeout(() => {
-      dot.remove();
-    }, 100);
-  }
+  //   setTimeout(() => {
+  //     dot.remove();
+  //   }, 100);
+  // }
 }
