52ccd7ce8b2f6529be3b56b315e56e3af5ee6b3a
yurididomenico <yurididomenico93@gmail.com>
2024-06-18 08:39:20 +0200

    Hotfixes
    Modified listLibraries description and added the Java icon to About page

--- src/app/services/about-description.service.ts
+++ src/app/services/about-description.service.ts
@@ -6,7 +6,7 @@ import { Injectable } from '@angular/core';
 export class AboutDescriptionService {
 
   description: string = "Mi chiamo Yuri e sono un appassionato del mondo della programmazione.\nHo 30 anni, sono fortemente motivato a perseguire questa strada e disponibile ad essere assunto con contratto di tirocinio o stage.\nNon vedo l\'ora di affinare le mie abilità di programmatore e rendere questa passione un vero e proprio lavoro.";
-  listLibraries: string = "In questi mesi ho speso il mio tempo imparando i seguenti linguaggi, framework e librerie:";
+  listLibraries: string = "Dal 2022 ad oggi ho speso il mio tempo imparando i seguenti linguaggi, framework, librerie e sistemi di versionamento:";
   languages: string = "HTML, CSS, JavaScript, PHP, Sass, Bootstrap, Laravel, Vue, Angular, Java, Git.";
 
 
