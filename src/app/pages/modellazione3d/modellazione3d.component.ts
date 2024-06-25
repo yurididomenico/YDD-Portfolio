f8c0abeee667a39d98a07d6db9d13ba417366f5d
yurididomenico <yurididomenico93@gmail.com>
2024-06-25 10:03:19 +0200

    Predisposizione e stampa carte progetti 3d

--- src/app/pages/modellazione3d/modellazione3d.component.ts
+++ src/app/pages/modellazione3d/modellazione3d.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Project_3D } from './modellazione3d.model';
+import { Projects3DService } from 'src/app/services/projects3-d.service';
 
 @Component({
   selector: 'app-modellazione3d',
@@ -6,5 +8,12 @@ import { Component } from '@angular/core';
   styleUrls: ['./modellazione3d.component.scss']
 })
 export class Modellazione3dComponent {
+  projects_3D: Project_3D[] = []
+
+  constructor(private projectsService: Projects3DService) {}
+
+  ngOnInit(): void {
+    this.projects_3D = this.projectsService.projects_3D_List;
+  }
 
 }
---
+++ src/app/pages/modellazione3d/modellazione3d.model.ts
@@ -0,0 +1,9 @@
+export class Project_3D {
+  constructor(
+    public title: string,
+    public description: string
+  ) {
+    this.title = title;
+    this.description = description;
+  }
+}
