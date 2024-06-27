28f74b1c6dc0d862e649aa2294049cce8c723906
yurididomenico <yurididomenico93@gmail.com>
2024-06-27 12:00:35 +0200

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,37 +10,5 @@ export class AppComponent implements OnInit {
   title = 'ydd-portfolio-angular';
 
   ngOnInit(): void {
-    // this.getMouseCoords();
   }
-
-
-  // getMouseCoords() {
-  //   const selectContainer = document.getElementsByClassName('container')[0];
-
-  //   window.addEventListener('mousemove', (event) => {
-  //     const mousePos = {
-  //       x: event.clientX,
-  //       y: event.clientY,
-  //     };
-
-  //     const dot = document.createElement('div');
-  //     dot.style.cssText = `
-  //       position: absolute;
-  //       height: 5px;
-  //       width: 5px;
-  //       background-color: #1daa1b;
-  //       border-radius: 5px;
-  //       top: ${mousePos.y + 10}px;
-  //       left: ${mousePos.x}px;
-  //       opacity: 0.5;
-  //       z-index: 1000;
-  //     `;
-
-  //     selectContainer.appendChild(dot);
-
-  //     setTimeout(() => {
-  //       dot.remove();
-  //     }, 300);
-  //   });
-  // }
 }
