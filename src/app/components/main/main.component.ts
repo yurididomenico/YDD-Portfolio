b1838c19af578a44dd28671a34376ac5444cb58e
yurididomenico <yurididomenico93@gmail.com>
2024-06-27 11:29:31 +0200

    .

--- src/app/components/main/main.component.ts
+++ src/app/components/main/main.component.ts
@@ -1,15 +1,13 @@
 import { Component, OnInit } from '@angular/core';
-import { NavigationEnd, Router } from '@angular/router';
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss'],
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
 
   constructor() {}
-
 
   ngOnInit(): void {
 
